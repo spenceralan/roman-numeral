97c1eafb6996b10618282423edaabd9ca6af77e6
Spencer Ruiz and Sara Schultz <spencer@>
2017-03-30 11:18:28 -0700

    add key for values 900, 400, and 40, and add return Nulla for input zero.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@ const romanNumerals = {
   5: "V",
   9: "IX",
   10: "X",
+  40: "XL",
   50: "L",
   90: "XC",
   100: "C",
+  400: "CD",
   500: "D",
   900: "CM",
   1000: "M",
@@ -18,6 +20,9 @@ const numeralKeys = Object.keys(romanNumerals);
 const romanNumeralGenerator = function(number) {
   if (number >= 4000) {
     return "Roman numerals can only count to 3,999."
+  };
+  if (number === 0) {
+    return "Nulla."
   };
   let romanNumeral = [];
   for (let i=(numeralKeys.length - 1); i >= 0 ; i --) {
