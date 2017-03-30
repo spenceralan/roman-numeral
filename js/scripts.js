2a2476eb02dbd91fefa9b13dd4a03bbfb56ca3f9
Spencer Ruiz and Sara Schultz <spencer@>
2017-03-30 10:06:46 -0700

    add functionality for additive roman numerals and pass first two specs.

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,33 @@ const romanNumerals = {
 const romanNumeralGenerator = function(number) {
   // debugger;
   let romanNumeral = [];
-  for (let i = 0; i < number; i ++) {
+  while ( number >= 1000) {
+    romanNumeral.push([romanNumerals["1000"]]);
+    number -= 1000;
+  };
+  while ( number >= 500) {
+    romanNumeral.push([romanNumerals["500"]]);
+    number -= 500;
+  };
+  while ( number >= 100) {
+    romanNumeral.push([romanNumerals["100"]]);
+    number -= 100;
+  };
+  while ( number >= 50) {
+    romanNumeral.push([romanNumerals["50"]]);
+    number -= 50;
+  };
+  while ( number >= 10) {
+    romanNumeral.push([romanNumerals["10"]]);
+    number -= 10;
+  };
+  while ( number >= 5) {
+    romanNumeral.push([romanNumerals["5"]]);
+    number -= 5;
+  };
+  while ( number >= 1) {
     romanNumeral.push([romanNumerals["1"]]);
+    number -= 1;
   };
   return romanNumeral.join("");
 };
